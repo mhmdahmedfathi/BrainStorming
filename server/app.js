528ffae8daae22a5f4a147c3668e8865da1f5d82
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-24 14:53:28 +0200

    enable cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
 
@@ -14,6 +15,7 @@ db().catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", require("./routes/users"));
 //app.use("/chat", require("./routes/chat"));
