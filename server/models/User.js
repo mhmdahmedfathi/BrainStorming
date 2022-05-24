260ea6640af04466a772d234e9184dd4d6fe9a8b
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-24 14:47:22 +0200

    make some addition to the user route

--- server/models/User.js
+++ server/models/User.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       minLength: 8,
-      maxLength: 30,
       required: true,
     },
   },
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt =  require("jsonwebtoken")
 
 /* AUTHENTICATION ROUTES */
 //router.post('/login', login);
@@ -10,25 +11,38 @@ router.post("/signup", async (req, res) => {
     const { name, username, password, password2 } = req.body;
 
     const result =
-      (await User.findOne({ username })) || (await User.findOne({ name }));
+      (await User.findOne({ username }));
 
     if (result) {
       return res.json({
+        Apistatues:false,
         error: "This user already exists",
       });
     }
     if (req.password !== req.password2) {
       return res.json({
+        Apistatues:false,
         error: "Passwords don't match",
       });
     }
 
-    req.body.password = await bcrypt.hash(password, 10);
+    req.body.password = await bcrypt.hash(password, 5);
 
-    await User.create(req.body);
+    const user = await User.create(req.body);
+    
+    data = {
+        username:user.username,
+        name:user.name
+    }
 
+    const token = jwt.sign({_id:user._id}, "LoginAccess" )
+
+    
     res.status(201).json({
-      success: "User signed up successfully",
+        Apistatues:true,
+        success: "User signed up successfully",
+        data:data,
+        token:token
     });
   } catch (error) {
     return res.json({
