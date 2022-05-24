5037c248f49f74da3498812d491dd018b46b8079
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-24 14:03:49 +0200

    signup complete

---
+++ server/routes/users.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs")
+
+/* AUTHENTICATION ROUTES */
+//router.post('/login', login);
+
+router.post('/signup', async(req,res)=>{
+    try {
+        const { name, username } = req.body;
+  
+        const result =
+          (await User.findOne({ username })) || (await User.findOne({ name }));
+  
+        if (result) {
+          return res.json({
+            error: "This user already exists",
+          });
+        }
+  
+        req.body.password = await bcrypt.hash(password, 10);
+  
+        await User.create(req.body);
+  
+        res.status(201).json({
+          success: "User signed up successfully",
+        });
+      } catch (error) {
+        return res.json({
+          error: error.message,
+        });
+      }
+    },
+);
+
+module.exports = router;
