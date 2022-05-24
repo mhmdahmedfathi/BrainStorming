321bcea553f716ee89a41c0118e854bf3ca6337c
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-24 15:43:53 +0200

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 
 /* AUTHENTICATION ROUTES */
 
