8bd719a460af4cc01d51ef0dbe1d2d98edb2d7d9
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-05-26 20:48:45 +0200

    add some comments to the files

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,40 +7,40 @@ const User = require("../models/User");
 
 router.post("/signup", async (req, res) => {
   try {
-    const { name, username, password, password2 } = req.body;
+    const { username, password, password2 } = req.body;
 
     const result = await User.findOne({ username });
 
-    if (result) {
+    if (result) { //if username found return error
       return res.status(400).json({
         ApiStatus: false,
         error: "This user already exists",
       });
     }
 
-    if (password !== password2) {
+    if (password !== password2) { //compare between password and confirm password and return error if not match
       return res.status(400).json({
         ApiStatus: false,
         error: "Passwords don't match",
       });
     }
 
-    req.body.password = await bcrypt.hash(password, 10);
+    req.body.password = await bcrypt.hash(password, 10); //to increase security by encrypting the password with solt 10
 
-    const user = await User.create(req.body);
+    const user = await User.create(req.body); //save the new account
 
     data = {
       username: user.username,
       name: user.name,
     };
 
-    const token = jwt.sign({ username: user.username }, "LoginAccess");
+    const token = jwt.sign({ username: user.username }, "LoginAccess"); //make a token by signing information with the secret and pass it to the front
 
-    res.status(201).json({
-      ApiStatus: true,
-      success: "User signed up successfully",
-      data,
-      token,
+    res.status(201).json({ //return to the front
+      ApiStatus: true, //indecates that their is no error
+      success: "User signed up successfully", //success message
+      data, //data of the user to be used in front
+      token, //token to be put in localstorage
     });
   } catch (error) {
     return res.status(500).json({
@@ -63,7 +63,7 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password); //compare the encrypted password from database with encrypted password provided from the user 
 
     if (!isMatch) {
       return res.json({
