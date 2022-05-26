52f01955a3d951ac9a3bd937561f98050ce51908
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-26 23:29:16 +0200

    Added comments to the frontend and refactored the code

--- Brainstorming/js/chat.js
+++ Brainstorming/js/chat.js
@@ -1,10 +1,33 @@
+// a list of all the available room names
+const availableRoom = [
+  "studying",
+  "traveling",
+  "gathering",
+  "learning",
+  "shopping",
+  "playing",
+  "scripting",
+  "reading",
+];
+
+// get the token from the local storage
 const token = localStorage.getItem("token");
 
+// if there is no token in the local storage, redirect to the login page
 if (!token) {
   alert("You are not logged in");
   location.href = "./login.html";
 }
 
+// get the room name from the url
+const room = new URLSearchParams(window.location.search).get("room");
+
+// if the room is not found or the room is not available, redirect to the groups page
+if (!room || !availableRoom.includes(room)) {
+  location.href = "./service.html";
+}
+
+// get the username from the local storage
 const { username } = JSON.parse(localStorage.getItem("user-data"));
 
 const topicTitle = document.querySelector("#topic-title");
@@ -13,17 +36,13 @@ const onlineCount = document.querySelector("#online-count");
 const messagesList = document.querySelector("#messages-list");
 const chatForm = document.querySelector("#chat-form");
 
-const room = new URLSearchParams(window.location.search).get("room");
+topicTitle.innerText = room; // set the topic title
+roomTitle.innerText = room + " chat room"; // set the room title
+onlineCount.innerText = 0; // set the online count to 0
 
-if (!room) {
-  location.href = "./service.html";
-}
-
-topicTitle.innerText = room;
-roomTitle.innerText = room + " chat room";
-onlineCount.innerText = 0;
-
-const parseMessage = (message) => {
+// this function parses the message in the HTML template and returns it to be appended to the messages list
+function parseMessage(message) {
+  // get the user's name initial characters
   const names = message.name.split(" ");
   const nameInitials =
     names.length > 1
@@ -31,6 +50,8 @@ const parseMessage = (message) => {
       : names[0][0] + names[0][1];
 
   if (message.username === username) {
+    // if the message is sent by the logged in user
+
     return `<div class="chat-message-right pb-4">
       <div class="d-flex flex-column">
           <div 
@@ -41,16 +62,18 @@ const parseMessage = (message) => {
           </div>
         </div>
       <div class="flex-shrink-1 bg-light rounded pb-2 px-3">
-        <div class="text-muted small font-italic">
-          ${message.time}
-        </div>
-        <div class="font-weight-bold mb-1">
+        <div class="font-weight-bold py-1">
           ${message.name} | ${message.username}
         </div>
-        ${message.message}
+        <p>${message.message}</p>
+        <small class="text-muted text-right small d-block font-italic">
+          ${message.time}
+        </small>
       </div>
     </div>`;
   } else {
+    // if the message is sent by another user
+
     return `<div class="chat-message-left pb-4">
       <div class="d-flex flex-column">
           <div 
@@ -61,18 +84,19 @@ const parseMessage = (message) => {
           </div>
         </div>
       <div class="flex-shrink-1 bg-light rounded pb-2 px-3">
-        <div class="text-muted small font-italic">
-          ${message.time}
-        </div>
-        <div class="font-weight-bold mb-1">
+        <div class="font-weight-bold py-1">
           ${message.name} | ${message.username}
         </div>
-        ${message.message}
+        <p>${message.message}</p>
+        <small class="text-muted text-right small d-block font-italic">
+          ${message.time}
+        </small>
       </div>
     </div>`;
   }
-};
+}
 
+// this function parses the notification in the HTML template and returns it to be appended to the messages list
 const parseNotification = (notification) => {
   return `<div class="chat-message-center pb-4">
             <div class="flex-shrink-1 bg-light rounded py-2 px-3 ml-3">
@@ -81,45 +105,82 @@ const parseNotification = (notification) => {
           </div>`;
 };
 
-document.addEventListener("DOMContentLoaded", () => {
+// wait for the page to load
+window.addEventListener("load", () => {
+  // create a socket connection
   const socket = io("http://localhost:8080", {
     transports: ["websocket"],
     auth: {
-      AccessToken: token,
+      AccessToken: token, // send the token to the server
     },
   });
 
+  // when the socket connection is established, join the room
   socket.on("connected", () => {
     socket.emit("joinRoom", room);
   });
 
+  // when there is a message from the server
   socket.on("message", (response) => {
     if (response.type === "joined-room") {
+      // if the user joined the room
+
+      // parse the notification that a new user joined the room
       messagesList.innerHTML += parseNotification(response.notification);
+
+      // update the online count
       onlineCount.innerText = response.onlineUsers;
+
+      // scroll to the bottom of the messages list
       messagesList.scrollTop = messagesList.scrollHeight;
     } else if (response.type === "room-messages") {
+      // if the user received the room messages
+
+      // update the online count
       onlineCount.innerText = response.onlineUsers;
+
+      // parse the messages
       response.messages.forEach((message) => {
         messagesList.innerHTML += parseMessage(message);
       });
     } else if (response.type === "new-message") {
+      // if the user received a new message
+
+      // parse the message
       messagesList.innerHTML += parseMessage(response.message);
+
+      // scroll to the bottom of the messages list
       messagesList.scrollTop = messagesList.scrollHeight;
     } else if (response.type === "left-room") {
+      // if a user left the room
+
+      // parse the notification that a user left the room
       messagesList.innerHTML += parseNotification(response.notification);
+
+      // update the online count
       onlineCount.innerText = response.onlineUsers;
+
+      // scroll to the bottom of the messages list
       messagesList.scrollTop = messagesList.scrollHeight;
     }
   });
 
+  // listen for a submit event on the chat form
   chatForm.addEventListener("submit", (e) => {
+    // prevent the page from reloading when the form is submitted
     e.preventDefault();
-    const message = e.target.elements.message.value;
 
-    socket.emit("message", { room, message });
+    // get the message from the input field and trim it
+    const message = e.target.elements.message.value.trim();
 
-    e.target.elements.message.value = "";
-    e.target.elements.message.focus();
+    // if the message is not empty
+    if (message) {
+      // send the message and the room name to the server
+      socket.emit("message", { room, message });
+
+      // clear the input field after the message is sent to the server
+      e.target.elements.message.value = "";
+      e.target.elements.message.focus();
+    }
   });
 });
--- Brainstorming/js/index.js
+++ Brainstorming/js/index.js
@@ -6,54 +6,77 @@ const logoutNavLink = document.querySelector("#logout-nav-link");
 const registerForm = document.querySelector("#register-form");
 const loginForm = document.querySelector("#login-form");
 
+// wait for the page to finish loading
 window.addEventListener("load", () => {
+  // get the token from the local storage
   const token = localStorage.getItem("token");
+
+  // get the user data from the local storage
   const userData = localStorage.getItem("user-data");
   let username = "";
+
+  // if there is a user data in the local storage, the get the username
   if (userData) {
     username = JSON.parse(userData).username;
   }
+
+  // check if the page is the register page or the login page
   const isRegisterPage = window.location.href.includes("register");
   const isLoginPage = window.location.href.includes("login");
 
+  // if the page is neither the register page nor the login page
   if (!isRegisterPage && !isLoginPage) {
     if (token) {
-      registerNavLink.style.display = "none";
-      loginNavLink.style.display = "none";
-      userNavLink.style.display = "block";
-      userNavLink.innerText = username;
-      logoutNavLink.style.display = "block";
+      // if there is a token in the local storage
+      registerNavLink.style.display = "none"; // hide the register nav link from the nav bar
+      loginNavLink.style.display = "none"; // hide the login nav link from the nav bar
+      userNavLink.style.display = "block"; // show the user nav link in the nav bar
+      userNavLink.innerText = username; // set the user nav link to the username of the logged in user
+      logoutNavLink.style.display = "block"; // show the logout nav link in the nav bar
     } else {
-      registerNavLink.style.display = "block";
-      loginNavLink.style.display = "block";
-      userNavLink.style.display = "none";
-      logoutNavLink.style.display = "none";
+      // if there is no token in the local storage
+      registerNavLink.style.display = "block"; // show the register nav link in the nav bar
+      loginNavLink.style.display = "block"; // show the login nav link in the nav bar
+      userNavLink.style.display = "none"; // hide the user nav link from the nav bar
+      logoutNavLink.style.display = "none"; // hide the logout nav link from the nav bar
     }
   }
 
+  // if the page is the register page or the login page, and the user is logged in, redirect to the home page
   if (token && (isRegisterPage || isLoginPage)) {
     alert("You are already logged in");
     location.href = "./index.html";
   }
 });
 
+// listen for a click on the logout nav link
 logoutNavLink?.addEventListener("click", () => {
+  // remove the token from the local storage
   localStorage.removeItem("token");
+
+  // remove the user data from the local storage
   localStorage.removeItem("user-data");
+
   alert("You have been logged out");
+
+  // redirect to home page
   location.href = "./index.html";
 });
 
+// listen for the submit event on the register form
 registerForm?.addEventListener("submit", (e) => {
+  // prevent the page from reloading after the form is submitted
   e.preventDefault();
 
+  // the body of the request contains the data from the form and trim the whitespace
   const body = {
-    name: registerForm["name"].value,
-    username: registerForm["username"].value,
-    password: registerForm["password"].value,
-    password2: registerForm["password2"].value,
+    name: registerForm["name"].value.trim(),
+    username: registerForm["username"].value.trim(),
+    password: registerForm["password"].value.trim(),
+    password2: registerForm["password2"].value.trim(),
   };
 
+  // make a post request to the signup endpoint with the body of the request to the server
   fetch("http://localhost:8080/users/signup", {
     method: "POST",
     headers: {
@@ -63,31 +86,47 @@ registerForm?.addEventListener("submit", (e) => {
   })
     .then((res) => res.json())
     .then((responseData) => {
+      // get the response data
       const { data, ApiStatus, token, error } = responseData;
 
       if (ApiStatus) {
+        // if the server response is successfully registered
+
+        // set the token in the local storage
         localStorage.setItem("token", token);
+
+        // set the user data in the local storage
         localStorage.setItem("user-data", JSON.stringify(data));
+
+        // reset the register form
         registerForm.reset();
+
+        // redirect to home page
         window.location.href = "./index.html";
       } else {
+        // if the server response is not successfully registered, show the error message
         alert(error);
       }
     })
     .catch((err) => {
+      // if there is an error, show the error message
       alert("An error happen while process your request. Please try again");
       console.log(err);
     });
 });
 
+// listen for the submit event on the login form
 loginForm?.addEventListener("submit", (e) => {
+  // prevent the page from reloading after the form is submitted
   e.preventDefault();
 
+  // the body of the request contains the data from the form and trim the whitespace
   const body = {
-    username: loginForm["username"].value,
-    password: loginForm["password"].value,
+    username: loginForm["username"].value.trim(),
+    password: loginForm["password"].value.trim(),
   };
 
+  // make a post request to the login endpoint with the body of the request to the server
   fetch("http://localhost:8080/users/login", {
     method: "POST",
     headers: {
@@ -97,18 +136,30 @@ loginForm?.addEventListener("submit", (e) => {
   })
     .then((res) => res.json())
     .then((responseData) => {
+      // get the response data
       const { data, ApiStatus, token, error } = responseData;
 
       if (ApiStatus) {
+        // if the server response is successfully logged in
+
+        // set the token in the local storage
         localStorage.setItem("token", token);
+
+        // set the user data in the local storage
         localStorage.setItem("user-data", JSON.stringify(data));
+
+        // reset the login form
         loginForm.reset();
+
+        // redirect to home page
         window.location.href = "./index.html";
       } else {
+        // if the server response is not successfully logged in, show the error message
         alert(error);
       }
     })
     .catch((err) => {
+      // if there is an error, show the error message
       alert("An error happen while process your request. Please try again");
       console.log(err);
     });
